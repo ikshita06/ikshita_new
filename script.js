055849c5ae5f39fa6c9c05fcc921cd41d56e69b9
Ikshita Gaur <ikshitagaur@Ikshitas-MacBook-Air.local>
2024-07-05 03:54:04 +0530

    hehe

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Product Designer . . ." , "UI/UX Wizard . . ." , "UX Auditor . . ." , "Designer . . ." , "UX Researcher . . ." , "Empath . . ."],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
